3510638d6d7459dec953dad7eb845196c1848946
Demel Gezachew Getie <demelgizachewu@gmail.com>
2025-12-01 20:07:52 +0300

    Header and Footer of Netflix Clone

--- netflix-react-app/src/App.jsx
+++ netflix-react-app/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <div>
-      <p>Hello Netflix</p>
-    </div>
-  )
+    <>
+      <Home />
+    </>
+  );
 }
 
-export default App
+export default App;
