e87a4448b9d043828175cc6a9457db8005111ebd
Demel Gezachew Getie <demelgizachewu@gmail.com>
2025-12-12 10:28:45 +0300

    fix: correct casing of Banner.css import

--- netflix-react-app/src/Component/Banner/Banner.jsx
+++ netflix-react-app/src/Component/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "../../Utils/axios"
 import requests from "../../Utils/requests";
-import "./Banner.css";
+import "./banner.css";
 
 function truncate(str, n) {
   return str?.length > n ? str.substr(0, n - 1) + "..." : str;
