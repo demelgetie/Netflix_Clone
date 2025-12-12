8e1186c215209bb5f8bd60cb81d513086ccb92eb
Demel Gezachew Getie <demelgizachewu@gmail.com>
2025-12-12 13:13:32 +0300

    vit

--- netflix-react-app/vite.config.js
+++ netflix-react-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Netflix_Clone/netflix-react-app/'
+  base: '/Netflix_Clone/'
   })
