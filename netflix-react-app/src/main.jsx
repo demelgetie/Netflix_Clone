12cfdaf6a33b1d04a05ed8e356c87ceb48712708
Demel Gezachew Getie <demelgizachewu@gmail.com>
2025-12-13 10:26:03 +0300

    final

--- netflix-react-app/src/main.jsx
+++ netflix-react-app/src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
 
-
-// https://deml.netlify.app/
+// https://app.netlify.com/projects/demel-net-flix-clone/
